8b130f4d2ef92e9b44b5c8c45383195b7ccc97de
Arjun0514 <arunchavan6828@gmail.com>
2025-12-27 15:31:40 +0530

    Refactor countSum function for subset sum calculation.. Removed extra variable currentsum

--- DP/countOfSubsetSum/usingMemorization.cxx
+++ DP/countOfSubsetSum/usingMemorization.cxx
@@ -3,31 +3,27 @@
 using namespace std;
 
 
-int countSum(vector<int> &vec, int target,int n,int currentSum,vector<vector<int>>&memo){
-    if(n==0){
-       return (target==currentSum);
-    }
-    
-    if(memo[n][currentSum]!=-1){
-        return memo[n][currentSum];
+int countSum(vector<int> &vec,int n,int target, vector<vector<int>>&memo){
+    if(target==0)
+      return 1;
+    if(n==0)
+       return 0;
+    if(memo[n][target]!=-1){
+        return memo[n][target];
     }
     int pick=0;
-    if(currentSum+vec[n-1]<=target){
-       pick=countSum(vec,target,n-1,currentSum+vec[n-1],memo);
+    if(vec[n-1]<=target){
+       pick=countSum(vec,n-1,target-vec[n-1],memo);
     }
-    int notPick=countSum(vec,target,n-1,currentSum,memo);
-    return memo[n][currentSum]=(pick+notPick);
-      
-
+    int notPick=countSum(vec,n-1,target,memo);
+    return memo[n][target]=(pick+notPick);
 }
 
 int main(){
-vector<int> vec={3,7,4,6,3};
-int target=10;
-int sum=0;
+vector<int> vec={1,1,1};
+int target=0;
 vector<vector<int>> memo(vec.size()+1,vector<int>(target+1,-1));
-memo[0][0]=1;
-cout<<countSum(vec,target,vec.size(),sum,memo);
+cout<<countSum(vec,vec.size(),target,memo);
 
 return 0;
 }

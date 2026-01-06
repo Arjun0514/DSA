1e87ce760b6ed8be49290eb3b000785f16429da3
Arjun0514 <arunchavan6828@gmail.com>
2026-01-06 15:48:48 +0530

    Add memoized LCS subsequence printing function
    Implement a function to print the longest common subsequence using memoization.

---
+++ DP/LCSPattern/LCSPrintSubsequence/usingMemo.cxx
@@ -0,0 +1,45 @@
+/*Printing LCSubsequence
+s1=ABCDGH
+s2=AEDFHR
+
+solution:-
+I know recursive approach for finding count of LC subsequence and LC substring
+-base case if any string becomes empty return str
+-if m and n matches then 1+rec(m-1,n-1) return str.push_back(s1[m-1]) 
+-or call max((m-1,n),(m,n-1))
+
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string printSubSequence(string &s1, string &s2,int m, int n, vector<vector<string>> &memo ){
+  if(m==0 || n==0)
+	  return "";
+	  
+  if(memo[m][n]!="-1")
+     return memo[m][n];
+  if(s1[m-1]==s2[n-1]){
+	 return memo[m][n]=printSubSequence(s1,s2,m-1,n-1,memo)+s1[m-1];
+  }
+  else{
+      string res1=printSubSequence(s1,s2,m,n-1,memo);
+	  string res2=printSubSequence(s1,s2,m-1,n,memo);
+	  return memo[m][n]=res1.size()>res2.size() ? res1:res2;
+  }
+}
+
+int main(){
+   string s1="ABCDGH";
+   string s2="AEDFHR";
+   
+   int m=s1.size();
+   int n=s2.size();
+   
+   vector<vector<string>> memo(m+1,vector<string>(n+1,"-1"));
+   cout<<printSubSequence(s1,s2,m,n,memo);
+   return 0;
+}
